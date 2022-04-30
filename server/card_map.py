25a4f6a4639b127530f0c455d78ea772f8d7cf83
Chris Buonocore <chrisdistrict@gmail.com>
2022-04-30 17:42:40 -0400

    name is main

--- server/card_map.py
+++ server/card_map.py
@@ -11,3 +11,12 @@ def get_best_card(category, supported_cards):
     # TODO: returns the best card(s) to use based on the provided category
     # supported cards is a restricted list of cards based on the current user.
     return None
+
+
+
+# python card_map.py <- this command should run the below code.
+if __name__ == '__main__':
+
+    # test case 1 example
+    best_card = get_best_card("restaurants", ['usa_mastercard', 'usa_chase', 'usa_amex'])
+    print(best_card)
